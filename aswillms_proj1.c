3068f0d66254fd89e9ce73a31c1cd4119784b367
Andrew Willms <willms.andrew@gmail.com>
2015-10-04 00:42:20 -0400

    Completed localIP() and handleDisplayRequest()

--- aswillms_proj1.c
+++ aswillms_proj1.c
@@ -29,9 +29,10 @@ http://beej.us/guide/bgnet/output/html/singlepage/bgnet.html#select
 
 
 char *substringOf(int i,int j,char *ch);
+char *localIP();
+void handleDisplayRequest();
 
-
-int main()
+int main(int argc, char *argv[])
 {
     int i=0,j=2;
     char str[]="String";
@@ -39,54 +40,8 @@ int main()
 
     test=substringOf(i,j,str);
     printf("%s\n",test);
-	
-	struct ifaddrs *ifaddr, *ifa;
-	int family, s, n;
-	char host[NI_MAXHOST];
 
-	if (getifaddrs(&ifaddr) == -1) {
-		perror("getifaddrs");
-		exit(EXIT_FAILURE);
-	}
-
-	/* Walk through linked list, maintaining head pointer so we can free list later */
-
-	for (ifa = ifaddr, n = 0; ifa != NULL; ifa = ifa->ifa_next, n++) {
-		if (ifa->ifa_addr == NULL){
-			continue;
-		}
-
-		family = ifa->ifa_addr->sa_family;
-
-		/* Display interface name and family (including symbolic form of the latter for the common families) */
-
-		//printf("%-8s %s (%d)\n", ifa->ifa_name,(family == AF_PACKET) ? "AF_PACKET" :(family == AF_INET) ? "AF_INET" : (family == AF_INET6) ? "AF_INET6" : "???",family);
-
-		/* For an AF_INET* interface address, display the address */
-
-		if (family == AF_INET || family == AF_INET6) {
-			s = getnameinfo(ifa->ifa_addr,(family == AF_INET) ? sizeof(struct sockaddr_in) :sizeof(struct sockaddr_in6),host, NI_MAXHOST,NULL, 0, NI_NUMERICHOST);
-			if (s != 0) {
-				printf("getnameinfo() failed: %s\n", gai_strerror(s));
-				exit(EXIT_FAILURE);
-			}
-
-			printf("\t\taddress: <%s>\n", host);
-
-		} 
-		/*
-		else if (family == AF_PACKET && ifa->ifa_data != NULL) {
-			struct rtnl_link_stats *stats = ifa->ifa_data;
-
-			printf("\t\ttx_packets = %10u; rx_packets = %10u\n"
-					"\t\ttx_bytes   = %10u; rx_bytes   = %10u\n",
-					stats->tx_packets, stats->rx_packets,
-					stats->tx_bytes, stats->rx_bytes);
-		}
-		*/
-	}
-
-	freeifaddrs(ifaddr);
+	handleDisplayRequest();
     return 0;
 }
 
@@ -105,4 +60,45 @@ char *substringOf(int i,int j,char *ch)
     }
 
     return (char *)ch1;
+}
+
+// Determines the local IP address of the current process
+char *localIP(){
+		
+	struct ifaddrs *ifaddr, *ifa;
+	int family, s, n;
+	char host[NI_MAXHOST];
+	char *myIP = malloc (sizeof(char)*NI_MAXHOST);
+
+	if (getifaddrs(&ifaddr) == -1) {
+		perror("getifaddrs");
+		exit(EXIT_FAILURE);
+	}
+	/* Walk through linked list, maintaining head pointer so we can free list later */
+	
+	for (ifa = ifaddr, n = 0; ifa != NULL; ifa = ifa->ifa_next, n++) {
+		if (ifa->ifa_addr == NULL){
+			continue;
+		}
+
+		family = ifa->ifa_addr->sa_family;
+		
+		if (family == AF_INET) {
+			s = getnameinfo(ifa->ifa_addr,(family == AF_INET) ? sizeof(struct sockaddr_in) :sizeof(struct sockaddr_in6),host, NI_MAXHOST,NULL, 0, NI_NUMERICHOST);
+			if (s != 0) {
+				printf("getnameinfo() failed: %s\n", gai_strerror(s));
+				exit(EXIT_FAILURE);
+			}
+			if(strcmp("127.0.0.1",host)!=0){
+				//printf("My address is <%s>\n",host);
+				strcpy(myIP,host);
+			}
+		}
+	}
+	freeifaddrs(ifaddr);
+	return myIP;
+}
+// Prints out required information for DISPLAY instruction
+void handleDisplayRequest(){
+	printf("Andrew S. Willms (aswillms)\naswillms@buffalo.edu\nLocal Process IP:   <%s>\nLocal Process Port: %d\n",localIP(),0);
 }
